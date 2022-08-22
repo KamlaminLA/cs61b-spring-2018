c78f9c1eb475915710859a82b002fab20fbe55ed
JINLIN DENG <felixdeng703@gmail.com>
2022-08-21 23:49:32 -0700

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,10 @@
+public class HelloNumbers{
+    public static void main(String[]args) {
+        int x = 0, i = 1;
+        while (i <= 10){
+           System.out.println(x);
+           x = x + i;
+           i = i + 1; 
+        }
+    }
+}
