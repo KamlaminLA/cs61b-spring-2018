bad4d03daf0d284be184ee4232054ecffdb2f6c3
JINLIN DENG <felixdeng703@gmail.com>
2023-03-13 22:36:32 -0700

    fk bitch, it is my 5 times debug

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -10,16 +10,21 @@ public class TestArrayDequeGold {
             int randomNum = StdRandom.uniform(1, 1000);
             s1.addFirst(randomNum);
             s2.addFirst(randomNum);
+            String mess = "" + "addFirst(" + s2.get(0) + ")\n";
+            assertEquals(mess, s2.get(0), s1.get(0));
         }
         for(int i = 0; i < 600; i++) {
             Integer a = s1.removeFirst();
             Integer b = s2.removeFirst();
-            assertEquals(a, b);
+            String mess = "" + "removeFirst(" + b + ")\n";
+            assertEquals(mess, b, a);
         }
         for(int i = 0; i < 100; i++) {
             int randomNum = StdRandom.uniform(1, 1000);
             s1.addLast(randomNum);
             s2.addLast(randomNum);
+            String mess = "" + "addLast(" + s2.size() + ")\n";
+            assertEquals(mess, s2.size(), s1.size());
         }
         for(int i = 0; i < 200; i++) {
             Integer actual = s1.removeLast();
