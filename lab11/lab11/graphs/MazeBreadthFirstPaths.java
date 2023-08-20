79784e66057d608dbbb3163bf2e12a8bec84987d
JINLIN DENG <felixdeng703@gmail.com>
2023-08-20 00:11:37 -0700

    finished lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,6 @@
 package lab11.graphs;
-
+import java.util.PriorityQueue;
+import java.util.Queue;
 /**
  *  @author Josh Hug
  */
@@ -9,21 +10,51 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze maze;
+    // One dimensional coordinate of the source vertex
+    private int s;
+    // One dimensional coordinate of the target vertex
+    private int t;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> queue = new PriorityQueue<>();
+        queue.add(s);
+        marked[s] = true;
+        announce();
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            if (v == t) {
+                return;
+            }
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    queue.add(w);
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] = true;
+                    edgeTo[w] = v;
+                    announce();
+                }
+            }
+        }
+
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,41 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-
+    private Maze maze;
+    private boolean isFound = false;
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+        distTo[0] = 0;
+        edgeTo[0] = 0;
+        dfsHelper(maze, 0);
     }
 
     // Helper methods go here
+    private void dfsHelper(Maze m, int v) {
+        marked[v] = true;
+        announce();
+        for (int w : m.adj(v)) {
+            if (edgeTo[w] != v && marked[w]) {
+                announce();
+                isFound = true;
+            }
+            if (isFound) {
+                return;
+            }
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                announce();
+                distTo[w] = distTo[v] += 1;
+                dfsHelper(m, w);
+            }
+
+        }
+    }
 }
 
